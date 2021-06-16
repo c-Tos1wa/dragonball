de603e4f2d3d02ccf4366807fa37493d171665cc
Cristina <cristoshiwassaki@gmail.com>
2021-06-16 11:14:53 -0300

    inicio de CRUD

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.scss'
+import logo from './logo.png'
+
+function App() {
+  return (
+    <> 
+      <div className='logoBar'>
+        <img className='logo-img' src={logo} alt='Logo do anime Dragonball Z' />
+      </div>
+    </>
+    
+  );
+}
+
+export default App;
